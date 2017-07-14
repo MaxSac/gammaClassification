a62cc33841f5b51ceaf1a7c0e35f30e1040819ce
MaxSac <maximilian.sackel@udo.edu>
2017-07-14 13:24:46 +0200

    start order

--- programm/tree_optimizer/tree_optimizer.py
+++ programm/tree_optimizer/tree_optimizer.py
@@ -1,3 +1,7 @@
+'''
+It's a programm which search for the best settings for a Forest with given parameters. The parameters are given in a config.yaml file. The reesponse is a plot and a file with the scores of the different settings.
+'''
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -6,31 +10,38 @@ from sklearn.model_selection import GridSearchCV
 import yaml
 plt.style.use('ggplot')
 
-with open('config.yaml') as f:
+'''
+Open and read the information from the *.yaml file.
+'''
+with open('./config.yaml') as f:
 	CONF = yaml.load(f)
-
 DATA = pd.read_pickle(CONF['path'])
 
+'''
+Make a gridsearch over given parameters and fit the different models.
+'''
 Tree = RandomForestClassifier(**CONF['set_params'])
-
 clf = GridSearchCV(
 		Tree, 
 		param_grid= CONF['loop_params'], 
 		**CONF['grid_params']
 		)
-
 clf.fit(DATA.drop('label', axis=1), DATA.label)
 
+'''
+Save the scorrings of the different parameters.
+'''
 best_esti = pd.DataFrame(clf.cv_results_)
 best_esti.to_pickle('bestEsti')
 
-FEATURE_1 = best_esti.param_max_depth
-FEATURE_2 = best_esti.param_max_features
-
+'''
+Plot two features on the axis with the third dimension the score.
+'''
+FEATURE_1 = best_esti['param_' + CONF['plot_feature']['feature_one']]
+FEATURE_2 = best_esti['param_' + CONF['plot_feature']['feature_two']]
 x = np.unique(FEATURE_1.values)
 y = np.unique(FEATURE_2.values)
 z = best_esti.mean_test_score.values.reshape(len(y),len(x))
-
 plt.imshow(z)
 plt.xticks(range(len(np.unique(FEATURE_1.values))), x)
 plt.yticks(range(len(np.unique(FEATURE_2.values))), y)
