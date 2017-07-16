1c9f316eb3b349223df4978c817c93d025e17688
MaxSac <maximilian.sackel@udo.edu>
2017-07-16 13:15:19 +0200

    nothing exciting, some changes model signinficance

--- programm/model_significance/test.py
+++ programm/model_significance/test.py
@@ -1,34 +1,11 @@
-import pandas as pd
-import numpy as np
-from fact.analysis import li_ma_significance, split_on_off_source_independent
-
-'''
-params: fitted estimator
-'''
-
-def model_significance(estimator, data):
-		data['gamma_prediction'] = Tree.predict_proba(data[feature])[:,1]
-		significance = []
-		for threshold in np.linspace(0.01, 0.99, 99):
-			on_data, off_data = split_on_off_source_independent(hadron_data.query('gamma_prediction >= 0.5'), theta2_cut=0.03)
-			#significance.append(li_ma_significance(len(on_data), len(off_data), 0.2))
-		return max(significance)
-
 from fact.io import read_h5py
 from sklearn.ensemble import RandomForestClassifier
-feature = ['conc_core',
-				'concentration_one_pixel',    
-				'concentration_two_pixel',
-				'leakage', 
-				'leakage2', 
-				'size', 
-				'width', 
-				'num_islands', 
-				'num_pixel_in_shower', 
-				'ph_charge_shower_max',
-				'ph_charge_shower_mean',
-				'ph_charge_shower_min',
-				'ph_charge_shower_variance']
+import yaml
+
+exec(open('model_significance.py').read())
+
+with open('/home/msackel/Desktop/gammaClassification/config/feature.yaml') as f:
+		feature = yaml.load(f)
 
 hadron_data = read_h5py(
 				'/home/msackel/Desktop/gammaClassification/data/raw_data/crab_precuts.hdf5',
@@ -45,5 +22,8 @@ hadron_data = read_h5py(
 
 crab = pd.read_pickle('/home/msackel/Desktop/gammaClassification/data/complete_Datasets/dataCrab')
 
-Tree = RandomForestClassifier().fit(crab.drop('label', axis=1), crab.label)
-model_significance(Tree, hadron_data)
+Tree = RandomForestClassifier(n_estimators=100, n_jobs=25).fit(crab.drop('label', axis=1), crab.label)
+print('max significance: ', model_significance(Tree, hadron_data))
+plot_significance(Tree, hadron_data)
+plot_on_off_ratio(Tree, hadron_data, 0.8)
+print('everything completed *.*')
